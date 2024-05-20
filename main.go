b232890cfc272a109bd3e85a16897eaeeb01dfba
Nick <garbalaunick@gmail.com>
2024-05-20 15:54:56 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,38 +1,45 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func binarySearch(arr [10]int, target int) int {
-	left := 0
-	right := len(arr) - 1
+func main() {
+	sls := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+	linearSearch(sls, 10)
+	binarySearch(sls, 10)
+}
 
-	for left <= right {
-		middle := left + (right-left)/2
+func linearSearch(arr []int, target int) bool {
+	for i, value := range arr {
+		if value == target {
+			fmt.Printf("linearSearch runs: %d | %s\n", i+1, "true")
+			return true
+		}
+	}
+	fmt.Println("linearSearch: not found")
+	return false
+}
 
-		if arr[middle] == target {
-			return middle
+func binarySearch(arr []int, target int) bool {
+	leftPointer := 0
+	rightPointer := len(arr) - 1
+	counter := 0
+
+	for leftPointer <= rightPointer {
+		counter++
+		middlePointer := (rightPointer + leftPointer) / 2
+
+		if arr[middlePointer] == target {
+			fmt.Printf("binarySearch runs: %d | %s\n", counter, "true")
+			return true
 		}
 
-		if arr[middle] > target {
-			right = middle - 1
-		}
-
-		if arr[middle] < target {
-			left = middle + 1
+		if arr[middlePointer] > target {
+			rightPointer = middlePointer - 1
+		} else {
+			leftPointer = middlePointer + 1
 		}
 	}
 
-	return -1
-}
-
-func main() {
-	arr := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-	findFive := binarySearch(arr, 5)
-	findHundread := binarySearch(arr, 5)
-
-	fmt.Println("findFive:", findFive)
-	fmt.Println("findHundread:", findHundread)
+	fmt.Println("binarySearch: not found")
+	return false
 }
